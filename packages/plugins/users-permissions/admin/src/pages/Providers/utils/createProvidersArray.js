e9f8d6ee8b63a6db6f4b330f1265134710184042
peterk <tiglek+u16office@gmail.com>
2022-05-12 17:46:58 +0900

    - add admin panel min comment

--- packages/plugins/users-permissions/admin/src/pages/Providers/utils/createProvidersArray.js
+++ packages/plugins/users-permissions/admin/src/pages/Providers/utils/createProvidersArray.js
@@ -1,10 +1,14 @@
 import { sortBy } from 'lodash';
 
+// [PK] fix icon not found error
+// Search from https://fontawesome.com/v5/search?q=user&m=free
+const _fasIcons = ['envelope', 'user', 'book'];
+
 const createProvidersArray = data => {
   return sortBy(
     Object.keys(data).reduce((acc, current) => {
       const { icon: iconName, enabled, subdomain } = data[current];
-      const icon = iconName === 'envelope' ? ['fas', 'envelope'] : ['fab', iconName];
+      const icon = _fasIcons.indexOf(iconName) >= 0 ? ['fas', iconName] : ['fab', iconName];
 
       if (subdomain !== undefined) {
         acc.push({ name: current, icon, enabled, subdomain });
