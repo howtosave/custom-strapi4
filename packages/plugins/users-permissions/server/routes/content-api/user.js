dd37692ee3e7043b9d9575e61552225bd52ae1a0
peterhs.kang <peterhs.kang@gmail.com>
2022-06-14 10:25:25 +0900

    add /users/me PUT route

--- packages/plugins/users-permissions/server/routes/content-api/user.js
+++ packages/plugins/users-permissions/server/routes/content-api/user.js
@@ -27,6 +27,14 @@ module.exports = [
     },
   },
   {
+    method: 'PUT',
+    path: '/users/me',
+    handler: 'user.me',
+    config: {
+      prefix: '',
+    },
+  },
+  {
     method: 'GET',
     path: '/users/:id',
     handler: 'user.findOne',
